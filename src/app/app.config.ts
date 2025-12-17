1e7e9b2f60a2e587b049377f725dc03daa2bca31
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2025-12-17 18:17:38 +0100

    project base fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
