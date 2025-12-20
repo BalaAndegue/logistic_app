72d8bcdc8380d4c446e2a305b3c557f912e931ff
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2025-12-20 14:42:55 +0100

    first commit

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,4 +1,3 @@
-// guards/role.guard.ts
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -7,11 +6,7 @@ import { UserRole } from '../models/user.model';
 export const roleGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  
-  // Récupération des rôles requis depuis la route
   const requiredRoles = route.data['roles'] as UserRole[];
-  
-  // Vérification si l'utilisateur a l'un des rôles requis
   if (requiredRoles && requiredRoles.length > 0) {
     const hasAccess = authService.hasRole(requiredRoles);
     
